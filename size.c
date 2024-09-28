3d751ad4b1f63a85cd9139851d3331386caad94c
Meenakshi Pal <pal.meenakshi@outlook.com>
2024-09-28 11:57:57 +1000

    size

---
+++ size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 5, b = 7, c = 3;
+    double average = 0.0;
+    char ch = 'e';
+
+    printf("on my system\n");   
+    printf("int is %lu bytes.\n", sizeof(int));
+    printf("long int is %lu bytes.\n", sizeof(long int));
+    printf("char is %lu bytes.\n", sizeof(ch));
+    printf("float is %lu bytes.\n", sizeof(float));
+    printf("double is %lu bytes.\n", sizeof(double));
+    printf("long double is %lu bytes.\n", sizeof(long double));
+    
+    return 0;
+   
+}
